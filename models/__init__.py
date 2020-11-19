28ae3c4151fee5baf45f4c2f7e1a462babef68f8
gfacciol <gfacciol@gmail.com>
2020-11-19 19:12:55 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,6 @@ Copyright (C) 2018-2019, Gabriele Facciolo <facciolo@cmla.ens-cachan.fr>
 """
 
 from .unet_model import *
-from .DnCNN import DnCNN, DnCNN_pretrained, CONV_BN_RELU
+from .DnCNN import BF_DnCNN, CONV_RELU, DnCNN, DnCNN_pretrained, CONV_BN_RELU
 from .FFDNet import FFDNet, FFDNet_pretrained_grayscale
 from .DCT import DCTlike
